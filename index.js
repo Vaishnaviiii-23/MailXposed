8069390318145d1f8a1cf41b4966718484d29c15
Vaishnaviiii-23 <vaish3229@gmail.com>
2025-08-01 14:37:49 +0530

    final

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 app.use(express.json());
 app.use('/api', breachRoutes);
-
+app.use(express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
