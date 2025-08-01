5ba5fe5fc3cdcb8f236b5e32525ac7cdf7741582
Vaishnaviiii-23 <vaish3229@gmail.com>
2025-08-01 11:04:17 +0530

    fix: correct db path

--- routes/breachRoutes.js
+++ routes/breachRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../server/db');
+const db = require('../db');
 
 // Get all breached data
 router.get('/breaches', (req, res) => {
