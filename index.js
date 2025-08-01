cae9237379dde938dd235e68595bd8a892e6b652
Vaishnaviiii-23 <vaish3229@gmail.com>
2025-08-01 11:41:06 +0530

    fixed

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.use('/api', breachRoutes);
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
